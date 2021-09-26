ad5bbe8b5e8adfe0b179bd8025ca0b419ad8ae3f
Chawwh <chachablueftw@gmail.com>
2021-09-26 11:17:23 -0600

    Assessment Completed

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 3: Coding practical questions with Jest
 
+
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -14,6 +16,15 @@
 // --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
 
 // a) Create a test with expect statements for each of the variables provided.
+// test written
+// OMG first test failed! Woohoo!~
+
+describe("fibonacci sequence", () => {
+    test("should take in a number greater than 2 and return an array that length containing the number of the fib sequence", () => {
+        expect(fibMkr(6)).toEqual([1, 1, 2, 3, 5, 8])
+        expect(fibMkr(10)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+    })
+})
 
 // Example input: 6
 // Expected output: [1, 1, 2, 3, 5, 8]
@@ -25,11 +36,51 @@
 
 // b) Create the function that makes the test pass.
 
+// Alrighty soooo Begin by declaring a function fibMkr that takes in a number as a parameter
+// Declare a variable numArr that equals an array the length of num -1
+// Declare a variable fibArr that equals [1,1] to start off our fibonacci sequence
+// Conditional if num is less than or equal to 1
+// Return "Enter a number greater than 1"
+// Else for loop for iteration
+// add up fibArr[i] and the index directly before it and push the sum to fibArr
+// Return fibArr
+
+
+const fibMkr = (num) => {
+    let numArr = []
+    numArr.length = num - 1
+    let fibArr = [1,1]
+    if(num === 1){
+        return "Enter a number greater than 1."
+    } else {
+   for(let i=1; i < numArr.length; i++){
+      fibArr.push(fibArr[i-1] + fibArr[i])
+   }
+   return fibArr
+  }
+}
+  console.log(fibMkr(6));
+
+//   IT PASSES!!! YES!!! I'm slightly unhappy with my code because any number less than or equal to 0 results in an error which I guess is fine because an array can't have negative indexes anyway
+// This code was extremely challenging for me because I had no prior knowledge of what the heck liar focaccia even is But I still pushed through!
 
 
 // --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
 
 // a) Create a test with expect statements for each of the variables provided.
+// if(test === "written"){
+    // run test to fail!
+// }
+
+
+describe('odds from least to greatest', () =>{
+    test('takes in an array and returns a new array of only odd numbers sorted from least to greatest', () =>{
+        expect(odds(fullArr1)).toEqual([-9, 7, 9, 199])
+        expect(odds(fullArr2)).toEqual([-823, 7, 23])
+    })
+})
+
+
 
 var fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
 // Expected output: [-9, 7, 9, 199]
@@ -39,12 +90,36 @@ var fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
 
 
 // b) Create the function that makes the test pass.
+// Declare a function called odds that takes in arrays as a parameter
+// Declare a variable oddOnly equal to an empty array for storing our odd numbers later
+// For loop for iteration
+// Conditional if array[i] % 2 does not equal 0
+// push array[i] into oddOnly
+// Return oddOnly sorted from least to greatest AND filtering typeof by 'number'
 
-
-
+const odds = (array) => {
+    let oddOnly = []
+    for(let i=0; i<array.length; i++){
+        if(array[i] % 2 !== 0){
+            oddOnly.push(array[i])
+        }
+    } return oddOnly.sort((a, b) => a - b).filter(value => typeof value === "number")
+}
+console.log(odds(fullArr1));
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
+// My brain is so fried that I forgot to add the tea bag to my tea. I've been drinking warm water for the last half hour.
+// Test written
+// Crazy=Genius && Test=Failed
+
+describe("add the sum of numbers", () => {
+    test("takes in an array and returns an array of the accumulating sum", () => {
+        expect(addSumNums(numbersToAdd1)).toEqual([2, 6, 51, 60])
+        expect(addSumNums(numbersToAdd2)).toEqual([0, 7, -1, 11])
+        expect(addSumNums(numbersToAdd3)).toEqual([])
+    })
+})
 
 var numbersToAdd1 = [2, 4, 45, 9]
 // Excpected output: [2, 6, 51, 60]
@@ -58,3 +133,17 @@ var numbersToAdd3 = []
 
 
 // b) Create the function that makes the test pass.
+
+// Declare a function called addSumNums that takes in an array as a parameter
+// return the array using .map for iteration
+// .map should start at index 0 then take the sum and assign it to the next value, then take the sum and add it with that value
+
+const addSumNums = (array) => {
+  return array.map((sum => value => sum += value)(0))
+}
+console.log(addSumNums(numbersToAdd2));
+
+// #3 made me want to cry
+// I had nested for loops and if statements at first and I was like EwMG so chaotic and messy! SOooOo I did a google research deep dive and found .map can use math properties! Thanks Stackoverflow!
+
+// I'm not as proud of my assessment this week as I've been the last 2 weeks... I think I'm gonna go take a nap then study more...
--- interview-questions.md
+++ interview-questions.md
@@ -9,59 +9,62 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is `this` in JavaScript?
 
-  Your answer:
+  Your answer: `this` is a neat little tool we add infront of keys when we want to call on them in the same object or constructor in a class!
 
-  Researched answer:
+  Researched answer: `this` is a function's keyword that refers to the object it belongs to when in the local scope.
 
 
 
 2. What is React? Why would you use it?
 
-  Your answer:
+  Your answer: React is a javascript library that combines javascript, html, and css into the ultimate front-end developer tool. You would use it for creating user-end expeirences for apps!
 
-  Researched answer:
+  Researched answer: React is a free and open sourse javascript library used for building user interfaces gifted to us by the almighty Facebook.
 
 
 
 3. Which lifecycle method is required in a React class component?
 
-  Your answer:
+  Your answer: To the best of my knowledge I believe render() is the required lifestyle method in react because it is in charge of rendering everything in the react component. 
 
-  Researched answer:
+  Researched answer: Oh snap it actually is render! Cool! So render is the required method in react lifecycles because it is the method that outputs the html elements to be displayed!
 
 
 
 4. What is JSX? What is one notable difference between HTML and JSX?
 
-  Your answer:
+  Your answer: JSX is the flavorful combination of HTML and Javascript that ever so slightly alters the syntax of HTML and CSS whenever the languages use the same word for different functions. An example would be classes! Classes exist in both CSS and Javascript so inside of JSX we declare to CSS style classes with className.
 
-  Researched answer:
+  Researched answer: JSX is a syntax extension of Javascript. This little guy allows us to write html within javascript code (Only in React though).
 
 
 
 5. What is yarn? What file(s) are modified in a React application when you run yarn?
 
-  Your answer:
+  Your answer: yarn is a package manager developed by facebook! It allows us to run programs like React and Jest. As far as modified files go when running yarn, I know for a fact it adds yarn.lock and package.json to your working directory.
+<!-- The only yarn no one has to buy-->
 
-  Researched answer:
+  Researched answer: yarn is a newer package manager. Files affected by yarn are the node modules! Node modules contain a massive amount of files of code that honestly terrify me. Fun fact, node modules are ignored by git!
+  
 
 
 
 6. STRETCH: What is an anonymous function in JavaScript?
 
-  Your answer:
+  Your answer: I've never heard of an anonymous function before but my guess would be a function that doesn't have a name or a typical way to call it. It makes sense because when someone wants to remain anonymous they don't use their name!
 
-  Researched answer:
+  Researched answer: Anonymous functions are nameless functions that often are not accessible after its creation. They are typically used as arguments for other functions or functions that are immediately invoked for execution.
 
 
 ## Looking Ahead: Terms for Next Week
 
-1. Conditional rendering:
+1. Conditional rendering: React's equivelent of conditionals in javascript. Just like with javascript we can use if statements and conditional operators! woohoo!
 
-2. Props:
+2. Props: short for properties, props are very similar to objects or classes  as they allow us to pass information to a component. 
 
-3. JavaScript Events:
+3. JavaScript Events: Events are actions to an HTML element with javascript functions
 
-4. Object oriented programming:
+4. Object oriented programming: OOP is a style of programming that heavily utilizes classes and methods
 
-5. Ruby:
+5. Ruby: oh~ Ruby is a dynamic, open source, high-level programming language originally developed in Japan!
+
